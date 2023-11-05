199872b564c0e1f0354e30ccc9b9240263b606f3
avmonte <unstoppablehay@gmail.com>
2023-11-05 06:12:04 +0400

    minor fixes

--- tools.py
+++ tools.py
@@ -5,14 +5,17 @@ import numpy as np
 
 
 def vis(matrix):  # for visualize
-	im = cv2.merge([np.zeros(shape=matrix.shape, dtype=matrix.dtype), matrix, np.zeros(shape=matrix.shape, dtype=matrix.dtype)])
-	h, w, c = im.shape
+	h, w, c = matrix.shape
+	center = (h - 1) // 2
+	max_val = matrix[center, center]
+	im = cv2.merge([np.zeros(shape=(h, w, c), dtype=matrix.dtype), matrix, np.zeros(shape=(h, w, c), dtype=matrix.dtype)])
 
 	for i in range(h):
 		for j in range(w):
 			if im[i, j][1] < 0:
 				im[i, j] = [0, 0, abs(im[i, j][1])]
 
-			im[i, j] = im[i, j] * (255 / np.max(matrix))
+			im[i, j] = im[i, j] * (255 / max_val)
+			np.max()
 
 	cv2.imwrite(f"vis_{time()}.png", im)
