c6b34232ff2d23fdecc2ab11b41e7bd753fc444e
avmonte <unstoppablehay@gmail.com>
2023-12-21 21:30:43 +0400

    project3

--- codebase/prism_gaussian.py
+++ codebase/prism_gaussian.py
@@ -3,22 +3,20 @@ from tools import vis
 
 import numpy as np
 
-import numpy as np
-
 
 class KernelGenerator:
     def __init__(self, stdev):
         self.stdev = stdev
 
     def generate(self, alpha):
-        threshold = 0.999
+        threshold = 0.97
         alpha_rad = np.radians(alpha)  # Convert alpha to radians
         a = np.cos(alpha_rad)
         b = np.sin(alpha_rad)
 
-        amp = 1 / 150
+        amp = 1 / 100
 
-        gaussian = lambda x, y: amp * np.exp(-((a * x + b * y) ** 2) / (2 * 5 ** 2))
+        gaussian = lambda x, y: amp * np.exp(-((a * x + b * y) ** 2) / (2 * self.stdev ** 2))
 
         n = 7
         arr = np.zeros(shape=(n, n, 1), dtype=np.float64)
@@ -32,9 +30,9 @@ class KernelGenerator:
                     arr[i, j] = gaussian(x, y)
 
         print(np.sum(arr, dtype=np.float64), n)  # for debug
-        return arr / np.sum(arr, dtype=np.float64)
+        return arr / np.sum(arr, dtype=np.float64)  # normalize
 
 
-kernel = KernelGenerator(10).generate(135)
+kernel = KernelGenerator(3).generate(0)
 
 vis(kernel)
--- codebase/stage_1.py
+++ codebase/stage_1.py
@@ -34,17 +34,20 @@ def convert_to_binary_and_save(image):  # EXAMPLE: T7
     kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
     sharpened = cv2.filter2D(image, -1, kernel)  # cv2.filter2D(image, -1, kernel) can be used for a faster computation
 
-    blurred = cv2.GaussianBlur(sharpened, (5, 5), 0)
+    blurred = cv2.GaussianBlur(sharpened, (5, 5), 5)
 
     # To grayscale
     gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
-    # Apply automatic contrast
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    contrast_enhanced = clahe.apply(gray)
+    cv2.imwrite('beforeAC.jpg', gray)
+
+    # # Apply automatic contrast
+    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    # contrast_enhanced = clahe.apply(gray)
+
 
     # To binary
-    _, binary = cv2.threshold(contrast_enhanced, 127, 255, cv2.THRESH_BINARY)
+    _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
     return binary
 
@@ -52,8 +55,8 @@ def convert_to_binary_and_save(image):  # EXAMPLE: T7
 def apply_hough_transform(image):  # EXAMPLE: T17
     ## Edge detection
     # edges = cv2.Canny(blurred, 50, 150, apertureSize=3)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
     # Apply Hough Transform
     lines = cv2.HoughLines(thresh, 1, np.pi / 180, 200)
@@ -72,26 +75,27 @@ def apply_hough_transform(image):  # EXAMPLE: T17
             y2 = int(y0 - 1000 * (a))
             cv2.line(cimg, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
+    # cv2.imwrite('INI.jpg', cimg)
 
     # average of theta values from Hough Transform
-    theta_radians = np.mean(lines[:, 0][:, 1])
+    if lines is not None:
+        theta_radians = np.mean(lines[:, 0][:, 1])
 
-    # Convert theta to degrees
-    theta_degrees = math.degrees(theta_radians)
+        # Convert theta to degrees
+        theta_degrees = math.degrees(theta_radians)
 
-    # Calculate the angle of the line with the x-axis
-    angle = 90 - theta_degrees
-    # TODO: save globally
+        # Calculate the angle of the line with the x-axis
+        angle = 90 - theta_degrees
 
-    print("Average Theta (degrees):", angle, round(angle, 2))
+        print("Average Theta (degrees):", angle, round(angle, 2))
 
-    # Rotate the image if the angle < 10 degrees
-    if abs(angle) < 10:
-        # Get the dimensions of the image
-        height, width = image.shape[:2]
-        center = (width / 2, height / 2)
-        rotation_matrix = cv2.getRotationMatrix2D(center, -round(angle, 2), 1.0)
-        image = cv2.warpAffine(image, rotation_matrix, (width, height), borderValue=(255, 255, 255))
+        # Rotate the image if the angle < 10 degrees
+        if abs(angle) < 10:
+            # Get the dimensions of the image
+            height, width = image.shape[:2]
+            center = (width / 2, height / 2)
+            rotation_matrix = cv2.getRotationMatrix2D(center, -round(angle, 2), 1.0)
+            image = cv2.warpAffine(image, rotation_matrix, (width, height), borderValue=(255, 255, 255))
 
     return image
 
@@ -99,20 +103,20 @@ def apply_hough_transform(image):  # EXAMPLE: T17
 def main():
     ## Read
 
-    filename = 'MD.M108_p1.jpg'
+    filename = 'img5.jpg'
     im = cv2.imread(filename)
 
     ## Image Manipulation
 
     # im = remove_red_marks(im)
-    # im = convert_to_binary_and_save(im)
-    # im = apply_hough_transform(im)
+    im = convert_to_binary_and_save(im)
+    im = apply_hough_transform(im)
 
     ## Write
 
     # today = datetime.now().strftime("%d%m%y")
-    today = str(round(time()))[-6:]  # consistent for 11 days after dec 21st 2023
-    cv2.imwrite(f'CRS.TST.{today}.{filename.split(".")[0]}_bin.png', im)
-
+    # today = str(round(time()))[-6:]  # consistent for 11 days after dec 21st 2023
+    # cv2.imwrite(f'CRS.TST.{today}.{filename.split(".")[0]}_bin.png', im)
+    cv2.imwrite('imgtest1.jpg', im)
 
 main()
